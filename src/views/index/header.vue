2d18c7b31a385371f4097662a6985fa64359035d
coderyufengwei <jeff_in_guangdong@163.com>
2022-01-10 01:01:06 +0800

    初始化项目目录

---
+++ src/views/index/header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'header',
+    setup(){
+
+        return {
+
+        }
+    }
+})
+</script>
+<style scoped lang="less">
+
+</style>
---
+++ src/views/index/tags.vue
@@ -0,0 +1,20 @@
+<template>
+    <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'tags',
+    setup(){
+
+        return {
+
+        }
+    }
+})
+</script>
+<style scoped lang="less">
+
+</style>
